38350782b09b8ef64edf9c432ef97ee362e5723c
FlaviosSimonis <flavios.simonis@gmail.com>
2022-10-21 20:47:47 +0300

    changes for heroku

--- index.js
+++ index.js
@@ -112,6 +112,6 @@ app.post('/user/update', (req,res)=>{
   });
 });
 
-app.listen(3000, ()=>{
-  console.log('listening on port 3000');
+app.listen(process.env.PORT || 5000, ()=>{
+  console.log('listening on port');
 });
